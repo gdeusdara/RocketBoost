a49934470583929cce267e9ac81a0ef3240cb58c
Guilherme Deusdará <guibanci@gmail.com>
2023-02-12 16:55:19 -0500

    adding moviment to the rocket
    Signed-off-by: Guilherme Deusdará <guibanci@gmail.com>

--- Assets/Scripts/FlyRocket.cs
+++ Assets/Scripts/FlyRocket.cs
@@ -5,24 +5,45 @@ using UnityEngine;
 public class FlyRocket : MonoBehaviour
 {
     public float force = 1000f;
-
     Rigidbody rocket;
 
+    public KeyCode[] goLeftKeys = { KeyCode.A, KeyCode.LeftArrow };
+    public KeyCode[] goRightKeys = { KeyCode.D, KeyCode.RightArrow };
+
+    public float rotationVelocity = 10;
+
     // Update is called once per frame
-    void Start() {
+    void Start()
+    {
       rocket = GetComponent<Rigidbody>();
     }
 
+    void RotateRocket(KeyCode[] keys, float direction)
+    {
+      foreach (KeyCode key in keys)
+      {
+        if (Input.GetKey(key)) {
+          transform.Rotate(direction, 0, 0);
+        }
+      }
+    }
+
+    void BoostRocket() {
+      if (!Input.GetKey(KeyCode.Space)) return;
+
+      float myForce = force * Time.deltaTime;
+
+      float zForce = Mathf.Sin(transform.rotation.x) * myForce;
+      float yForce = Mathf.Sin(90 - transform.rotation.x) * myForce;
+
+      Vector3 forceApplied = new Vector3(0, yForce, zForce);
+      rocket.AddForce(forceApplied, ForceMode.Force);
+    }
 
     void FixedUpdate()
     {
-        // Debug.Log("Entrou aquiii");
-        float myForce = force * Time.deltaTime;
-
-        float zForce = Mathf.Sin(transform.rotation.x) * myForce;
-        float yForce = Mathf.Sin(90 - transform.rotation.x) * myForce;
-
-        Vector3 forceApplied = new Vector3(0, yForce, zForce);
-        rocket.AddForce(forceApplied, ForceMode.Force);
+        BoostRocket();
+        RotateRocket(goLeftKeys, rotationVelocity);
+        RotateRocket(goRightKeys, -rotationVelocity);
     }
 }
